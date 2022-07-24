0f810fe677befdd6d752c27f6f85d8f8492c35d4
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-07-24 12:15:02 +0200

    front aggiunto

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::middleware('auth')
     ->group(function(){
         Route::get('/home', 'HomeController@index')->name('home');
     });
+
+Route::get('{any?}',function(){
+    return view('guest.home');
+})->where('any','.*');
