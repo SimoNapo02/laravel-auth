54be6dc3b262e1d7b30b33f231d72b59755b386c
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-07-24 12:52:07 +0200

    fix create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,20 +4,22 @@
 @section('content')
 <div class="container margin-auto">
     <div class="card">
-        <div class="card-header text-uppercase">Crea nuovo post</div>
+        <div class="card-header text-uppercase ">Crea nuovo post</div>
         <div class="card-body">
             <form action="{{route('admin.posts.store')}}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="title">Titolo del post</label>
-                    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+                    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp"
+                        value="{{old('title')}}">
                 </div>
 
                 <label for="content">Inserisci il contenuto del post</label>
-                <textarea class="form-control" id="content" name="content" rows="7"></textarea>
+                <textarea class="form-control" id="content" name="content" rows="7">{{old('content')}}</textarea>
 
                 <div class="form-group form-check pt-4">
-                    <input type="checkbox" name="published" class="form-check-input" id="published">
+                    <input type="checkbox" name="published" class="form-check-input" id="published" {{old('published')
+                        ? 'checked' : "" }}>
                     <label class="form-check-label" for="published">Pubblico</label>
                 </div>
                 <button type="submit" class="btn btn-primary">Crea post</button>
